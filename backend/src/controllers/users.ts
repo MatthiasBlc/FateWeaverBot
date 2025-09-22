1aa2616661adb1b41586fd10b2f16f11649a5c83
matthias.bouloc <matthias.bouloc@univ-rouen.fr>
2025-09-22 10:29:54 +0200

    backend system corrections on sessions and TS

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,10 +1,9 @@
 import { RequestHandler } from "express";
 import createHttpError from "http-errors";
-import prisma from "../util/db";
+import { prisma } from "../util/db";
 import bcrypt from "bcrypt";
 
 export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
-
   try {
     const user = await prisma.user.findUnique({
       where: {
@@ -14,22 +13,25 @@ export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
         username: true,
         email: true,
       },
-    })
+    });
     res.status(200).json(user);
   } catch (error) {
     next(error);
   }
-
 };
 
 interface SignUpBody {
-  username?: string,
-  email?: string
-  password?: string
+  username?: string;
+  email?: string;
+  password?: string;
 }
 
-
-export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = async (req, res, next) => {
+export const signUp: RequestHandler<
+  unknown,
+  unknown,
+  SignUpBody,
+  unknown
+> = async (req, res, next) => {
   const username = req.body.username;
   const email = req.body.email;
   const passwordRaw = req.body.password;
@@ -46,10 +48,13 @@ export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = asy
       select: {
         username: true,
       },
-    })
+    });
 
     if (existingUsername) {
-      throw createHttpError(409, "Username already taken. Please choose a different one or log in instead.");
+      throw createHttpError(
+        409,
+        "Username already taken. Please choose a different one or log in instead."
+      );
     }
 
     const existingEmail = await prisma.user.findUnique({
@@ -59,14 +64,16 @@ export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = asy
       select: {
         email: true,
       },
-    })
+    });
 
     if (existingEmail) {
-      throw createHttpError(409, "A user with this email adress already exist. Please log in instead.");
+      throw createHttpError(
+        409,
+        "A user with this email adress already exist. Please log in instead."
+      );
     }
 
     const passwordHashed = await bcrypt.hash(passwordRaw, 10);
-
 
     const newUser = await prisma.user.create({
       data: {
@@ -78,22 +85,23 @@ export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = asy
 
     req.session.userId = newUser.id;
 
-
     res.status(201).json(newUser);
-
-
   } catch (error) {
     next(error);
   }
-
 };
 
 interface LoginBody {
-  username?: string,
-  password?: string,
+  username?: string;
+  password?: string;
 }
 
-export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async (req, res, next) => {
+export const login: RequestHandler<
+  unknown,
+  unknown,
+  LoginBody,
+  unknown
+> = async (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -112,7 +120,7 @@ export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async
         email: true,
         password: true,
       },
-    })
+    });
 
     if (!user) {
       throw createHttpError(401, "Invalid credentials");
@@ -121,27 +129,22 @@ export const login: RequestHandler<unknown, unknown, LoginBody, unknown> = async
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
-
       throw createHttpError(401, "Invalid credentials");
     }
 
     req.session.userId = user.id;
     res.status(201).json(user);
-
-
   } catch (error) {
-
     next(error);
   }
-
 };
 
 export const logout: RequestHandler = (req, res, next) => {
-  req.session.destroy(error => {
+  req.session.destroy((error) => {
     if (error) {
       next(error);
     } else {
       res.sendStatus(200);
     }
   });
-}
+};
